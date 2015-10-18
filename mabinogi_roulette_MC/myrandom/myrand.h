5e79bddbde5f64f99d05c5ed2573f8e0d7b8bbb8
dc1394 <hriver2014-github@memoad.jp>
2015-10-18 15:36:00 +0900

    最初のコミット

---
+++ mabinogi_roulette_MC/myrandom/myrand.h
@@ -0,0 +1,121 @@
+﻿/*! \file myrand.h
+    \brief 自作乱数クラスの宣言
+
+    Copyright © 2015 @dc1394 All Rights Reserved.
+    This software is released under the BSD 2-Clause License.
+*/
+
+#ifndef _MYRAND_H_
+#define _MYRAND_H_
+
+#pragma once
+
+#include <cstdint>  // for std::int32_t, std::uint_least32_t
+#include <random>   // for std::mt19937
+#include <vector>   // for std::vector
+#include <boost/range/algorithm.hpp>    // for boost::generate
+
+namespace myrandom {
+    //! A class.
+    /*!
+        自作乱数クラス
+    */
+    class MyRand final {
+        // #region コンストラクタ・デストラクタ
+
+    public:
+        //! A constructor.
+        /*!
+            唯一のコンストラクタ
+            \param min 乱数分布の最小値
+            \param max 乱数分布の最大値
+        */
+        MyRand(std::int32_t min, std::int32_t max);
+
+        //! A destructor.
+        /*!
+            デフォルトデストラクタ
+        */
+        ~MyRand() = default;
+
+        // #endregion コンストラクタ・デストラクタ
+
+        // #region メンバ関数
+
+        //!  A public member function.
+        /*!
+            [min, max]の閉区間で一様乱数を生成する
+        */
+        std::int32_t myrand()
+        {
+            return distribution_(randengine_);
+        }
+
+        // #endregion メンバ関数
+
+        // #region メンバ変数
+
+    private:
+        //! A private static member variable (constant expression).
+        /*!
+            初期乱数生成用のstd::vectorのサイズ
+        */
+        static std::vector<std::uint_least32_t>::size_type const SIZE = 64;
+
+        //! A private member variable.
+        /*!
+            乱数の分布
+        */
+        std::uniform_int_distribution<std::int32_t> distribution_;
+        
+        //! A private member variable.
+        /*!
+            乱数エンジン
+        */
+        std::mt19937 randengine_;
+
+        // #region 禁止されたコンストラクタ・メンバ関数
+
+        //! A private constructor (deleted).
+        /*!
+        デフォルトコンストラクタ（禁止）
+        */
+        MyRand() = delete;
+
+        //! A private copy constructor (deleted).
+        /*!
+            コピーコンストラクタ（禁止）
+        */
+        MyRand(const MyRand &) = delete;
+
+        //! A private member function (deleted).
+        /*!
+            operator=()の宣言（禁止）
+            \param コピー元のオブジェクト（未使用）
+            \return コピー元のオブジェクト
+        */
+        MyRand & operator=(const MyRand &) = delete;
+
+        // #endregion 禁止されたコンストラクタ・メンバ関数
+    };
+
+    MyRand::MyRand(std::int32_t min, std::int32_t max) :
+        distribution_(min, max)
+    {
+        // ランダムデバイス
+        std::random_device rnd;
+
+        // 初期化用ベクタ
+        std::vector<std::uint_least32_t> v(SIZE);
+
+        // ベクタの初期化
+        boost::generate(v, std::ref(rnd));
+
+        std::seed_seq seq(v.begin(), v.end());
+
+        // 乱数エンジン
+        randengine_ = std::mt19937(seq);
+    }
+}
+
+#endif  // _MYRAND_H_
