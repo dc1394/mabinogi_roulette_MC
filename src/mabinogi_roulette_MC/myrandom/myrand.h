fafa052f7012c0ebcc8ed996927a29ce71472e16
dc1394 <hriver2014-bitbucket@memoad.jp>
2018-10-11 14:01:29 +0900

    細かい修正

--- src/mabinogi_roulette_MC/myrandom/myrand.h
+++ src/mabinogi_roulette_MC/myrandom/myrand.h
@@ -10,11 +10,8 @@
 
 #pragma once
 
-#include <cstdint>                      // for std::int32_t, std::uint_least32_t
-#include <functional>                   // for std::ref
-#include <random>                       // for std::mt19937, std::random_device
-#include <vector>                       // for std::vector
-#include <boost/range/algorithm.hpp>    // for boost::generate
+#include <cstdint>  // for std::int32_t, std::uint_least32_t
+#include <random>   // for std::mt19937, std::random_device
 
 namespace myrandom {
     //! A class.
@@ -57,12 +54,6 @@ namespace myrandom {
         // #region メンバ変数
 
     private:
-        //! A private static member variable (constant expression).
-        /*!
-            初期乱数生成用のstd::vectorのサイズ
-        */
-        static std::vector<std::uint_least32_t>::size_type const SIZE = 8;
-
         //! A private member variable.
         /*!
             乱数の分布
@@ -92,7 +83,6 @@ namespace myrandom {
         //! A private member function (deleted).
         /*!
             operator=()の宣言（禁止）
-            \param コピー元のオブジェクト（未使用）
             \return コピー元のオブジェクト
         */
         MyRand & operator=(const MyRand &) = delete;
@@ -106,18 +96,8 @@ namespace myrandom {
         // ランダムデバイス
         std::random_device rnd;
 
-        // 初期化用ベクタ
-        std::vector<std::uint_least32_t> v(SIZE);
-
-        // ベクタの初期化
-        // 非決定的な乱数でシード列を構築する
-        boost::generate(v, std::ref(rnd));
-
-        // シード列を生成
-        std::seed_seq seq(v.begin(), v.end());
-
         // 乱数エンジン
-        randengine_ = std::mt19937(seq);
+        randengine_ = std::mt19937(rnd());
     }
 }
 
